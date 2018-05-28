1d404b3cb691b0158aaa9d772431c768cb302738
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-05-28 18:51:24 +0200

    Rename Elixir app to appsignal_terminal

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Wwdc16.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :wwdc16,
+    [app: :appsignal_terminal,
      version: "0.1.0",
      elixir: "~> 1.3.0",
      deps: deps]
